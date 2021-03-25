df1e8b8c882626f4ff540aa5f381766780bc140a
LugalKiEn <ulasik1996@gmail.com>
2021-03-26 01:27:44 +0300

    refactor: add BEM notation (not for all document) according to rule 2.1 Advanced in html-and-css.md

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ var addTask=function(){
     bindTaskEvents(listItem, taskCompleted);
 
     taskInput.value="";
+    listItem.classList.add("menu__item")
 
 }
 
@@ -85,7 +86,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit-task");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var containsClass=listItem.classList.contains("menu__item_active");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +100,8 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("menu__item_active");
+    listItem.classList.toggle("menu__item");
 };
 
 
